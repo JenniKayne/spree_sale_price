a21b649c631766bd29cea23b06cf445682eb2c2e
Wojtek <swierczynskiwojtek@gmail.com>
2018-01-29 11:38:44 +0100

    fixes

--- app/models/spree/order/checkout_decorator.rb
+++ app/models/spree/order/checkout_decorator.rb
@@ -3,10 +3,14 @@ Spree::Order::Checkout.class_eval do
   Spree::Order.state_machine.before_transition to: :complete, do: :save_line_items_final_sale
 
   def save_line_items_original_price
-    line_items.each(&:set_original_price)
+    line_items.each do |line_item|
+      update_column(:original_price, line_item.variant.original_price) if line_item.variant.present?
+    end
   end
 
   def save_line_items_final_sale
-    line_items.each(&:set_final_sale)
+    line_items.each do |line_item|
+      update_column(:final_sale, line_item.variant.final_sale) if line_item.variant.present?
+    end
   end
 end
--- db/migrate/20180129095242_add_original_price_to_line_items.rb
+++ db/migrate/20180129095242_add_original_price_to_line_items.rb
@@ -3,8 +3,10 @@ class AddOriginalPriceToLineItems < ActiveRecord::Migration[5.1]
     add_column :spree_line_items, :original_price, :decimal, precision: 10, scale: 2, null: false, default: 0
     add_column :spree_line_items, :final_sale, :boolean, default: false
     Spree::LineItem.all.each do |line_item|
-      line_item.update_column(:original_price, line_item.variant.original_price) if line_item.variant.present?
-      line_item.update_column(:final_sale, line_item.variant.final_sale) if line_item.variant.final_sale
+      if line_item.variant.present?
+        line_item.update_column(:original_price, line_item.variant.original_price)
+        line_item.update_column(:final_sale, line_item.variant.final_sale)
+      end
     end
   end
 end
