42185aa30a2ebbfe6426fc82b75696b628921c02
Wojtek <swierczynskiwojtek@gmail.com>
2018-01-29 11:46:07 +0100

    update_column to update, default values

--- db/migrate/20180129095242_add_original_price_to_line_items.rb
+++ db/migrate/20180129095242_add_original_price_to_line_items.rb
@@ -4,8 +4,8 @@ class AddOriginalPriceToLineItems < ActiveRecord::Migration[5.1]
     add_column :spree_line_items, :final_sale, :boolean, default: false
     Spree::LineItem.all.each do |line_item|
       if line_item.variant.present?
-        line_item.update_column(:original_price, line_item.variant.original_price)
-        line_item.update_column(:final_sale, line_item.variant.final_sale)
+        line_item.update_column(:original_price, 0)
+        line_item.update_column(:final_sale, false)
       end
     end
   end
